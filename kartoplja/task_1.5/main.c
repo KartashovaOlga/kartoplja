d516fba2360b43f528418e9d2635f5d9bbc24660
KartashovaOlga <kartashovaolga96@gmail.com>
2017-10-09 22:11:37 +0300

    Update main.c

--- kartoplja/task_1.5/main.c
+++ kartoplja/task_1.5/main.c
@@ -13,36 +13,46 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 
-/*
- * 
- */
+int getNumber(void);
+void pythagoreanTriplets(int length);
+
 int main(int argc, char** argv) {
-    
-    int leg1, leg2, hypotenuse, length;
-    
-    printf("Enter the max length of the triangle side: \n");
-    scanf("%d", &length);
-    
-    while(length < 0){
-       printf("Enter the max length of the triangle side: \n");
-       scanf("%d", &length); 
+    int length = getNumber();
+        
+    while(length < 0)
+    {
+      length = getNumber();
     }
     
-    printf("Leg 1  Leg 2  Hypotenuse\n");
+    pythagoreanTriplets(length);
     
-    for(hypotenuse = 1; hypotenuse <= length; hypotenuse++){
-        for(leg1 = 1; leg1 <= length; leg1++){
-            for(leg2 = 1; leg2 <= length; leg2++){
-                
-                if(hypotenuse*hypotenuse == ((leg1*leg1) + (leg2*leg2))){
+    return 0;
+}
+
+int getNumber(void){
+    int length;
+    printf("Enter the max length of the triangle side: \n");
+    scanf("%d", &length);
+    return length;
+}
+
+void pythagoreanTriplets(int length){
+    int leg1, leg2, hypotenuse;
+    
+    printf("Leg 1\tLeg 2\tHypotenuse\n");
+    
+    for(hypotenuse = 1; hypotenuse <= length; hypotenuse++)
+    {
+        for(leg1 = 1; leg1 <= length; leg1++)
+        {
+            for(leg2 = 1; leg2 <= length; leg2++)
+            {
+                if(hypotenuse*hypotenuse == ((leg1*leg1) + (leg2*leg2)))
+                {
                     printf("%d\t%d\t%d\n", leg1, leg2, hypotenuse);
                 }
             }
         }
     }
-
-    return (EXIT_SUCCESS);
 }
-
