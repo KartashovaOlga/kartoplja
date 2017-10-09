32e4df2ac63cf0471ae615e195653f1edd2b36bd
KartashovaOlga <kartashovaolga96@gmail.com>
2017-10-09 21:41:21 +0300

    Update main.c
    Need to improve output

--- kartoplja/task_1.3/main.c
+++ kartoplja/task_1.3/main.c
@@ -2,7 +2,7 @@
 Задача 3: «Вычисление экспоненты»
 Входные данные: вводятся с клавиатуры
 Выходные данные: выводятся на экран
-Напишите программу, которая оценивает значение е с заданной точностью е (0 <е < 1) с помощью ряда Тейлора. 
+Напишите программу, которая оценивает значение е с заданной точностью е (0 <е < 1). 
 Примечание. е = 2.718281828459...
 Указание. Изучите преобразование типов данных в языке Си.
 Входные данные: oдно действительное число е (0 <е< 1). 
@@ -11,13 +11,45 @@
 
 
 #include <stdio.h>
-#include <stdlib.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+int factorial(int i);
+float getNumber(void);
 
-    return (EXIT_SUCCESS);
+int main(int argc, char** argv) 
+{
+    float precision = getNumber();
+    
+    while(precision >= 1 || precision <= 0)
+    {
+        precision = getNumber();
+    }
+    
+    double temp = 1.0, exp = 1.0;   
+    int num = 1;
+    while(temp > precision)
+    {
+        temp = 1.0 / (double) factorial(num);
+        exp += temp ;
+        num++;
+    }
+    
+    printf("Exponent = %lf", exp);       
+
+    return 0;
 }
 
+int factorial(int i){
+    if(i <= 1)
+    {
+        return 1;
+    }
+    return i * factorial(i-1);
+}
+
+float getNumber(void)
+{
+    float i;  
+    printf("Enter the square's side length: \n");
+    scanf("%f", &i);
+    return i;
+}
