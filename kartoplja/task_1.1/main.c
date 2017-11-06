baebcfc386f780118b6ff8a9583af8beffca7fd6
KartashovaOlga <kartashovaolga96@gmail.com>
2017-11-06 10:41:27 +0300

    Updated main.c according to comments

--- kartoplja/task_1.1/main.c
+++ kartoplja/task_1.1/main.c
@@ -11,16 +11,38 @@
 #include <stdlib.h>
 
 size_t getNumber(void);
+void printSquare(const size_t length);
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
     size_t length = getNumber();
+     
+    printSquare(length);
+         
+    return (EXIT_SUCCESS);
+}
+
+size_t getNumber(void)
+{
+    size_t number;  
     
-    while(length > 80 || length < 0)
+    printf("Enter the square's side length:\n");
+    scanf("%u", &number);
+
+    while(number > 80 || number < 0)
     {
-       length = getNumber();
+       printf("You entered an invalid character\n");
+       printf("Enter the square's side length:\n");
+       
+       scanf("%u", &number);
     }
     
-    int i, j;
+    return number;
+}
+
+void printSquare(const size_t length)
+{
+  int i, j;
     
     for(i = 0; i < length; i++)
     {
@@ -30,28 +52,21 @@ int main(int argc, char** argv) {
             {
                 printf("*");
             }
-        }else
+        }
+        else
         {
             for(int j = 0; j < length; j++)
             {
                 if(j == 0 || j == length -1)
                 {
                     printf("*");
-                }else
+                }
+                else
                 {
                     printf(" ");
                 }
             }
         }
        printf("\n");
-    }
-     
-    return (EXIT_SUCCESS);
-}
-
-size_t getNumber(void){
-    size_t number;  
-    printf("Enter the square's side length: \n");
-    scanf("%u", &number);
-    return number;
+    }  
 }
