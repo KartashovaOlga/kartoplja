d4b4d8d2706c26e8ef3a7e84ae35237958fd1f44
KartashovaOlga <kartashovaolga96@gmail.com>
2018-08-20 22:56:53 +0300

    Updated the solution

--- kartoplja/task_3.1/main.c
+++ kartoplja/task_3.1/main.c
@@ -13,32 +13,18 @@
 
 
 char * readFromFile(void);
-int checkInfix(char *infix, int length);
-void writeToFile(char  *postfix, int length);
+bool checkInfix(const char *infix, const int length);
+void writeToFile(const char  *postfix, const int length);
+void generateReversePolishNotation(const char *infix, const int length);
 
 int main(int argc, char **argv)
 {
     int length;
-
     char *infix = readFromFile();
+    strcat(infix, ")");
     length = strlen(infix);
 
-    if(checkInfix(infix, length) > 0)
-    {
-        Stack_t *myStack = (Stack_t*)malloc(sizeof(char));
-        push(myStack, '(');
-
-        char *postfix = parseInput(myStack, infix, length);
-
-        writeToFile(postfix, length);
-
-        free(postfix);
-        free(myStack);
-    }
-    else
-    {
-        printf("File contains wrong string\n");
-    }
+    generateReversePolishNotation(infix, length);
 
     free(infix);
 
@@ -58,52 +44,43 @@ char * readFromFile(void)
     else
     {
         fseek(inputFile, 0, SEEK_END);
-        int size = ftell(inputFile);
+        int size = ftell(inputFile) + 1;
 
-        if(size == 0)
-        {
-            printf("File is empty\n");
-            fclose(inputFile);
-            return NULL;
-        }
-        else
-        {
-            fseek(inputFile, 0, SEEK_SET);
-            char *input = (char*)calloc(size, 1);
+         fseek(inputFile, 0, SEEK_SET);
+         char *input = (char*)calloc(size, sizeof(char));
 
-            fgets(input, size, inputFile);
+         fgets(input, size, inputFile);
+         fclose(inputFile);
 
-            strcat(input, ")");
-            fclose(inputFile);
-
-            return input;
-        }
+         return input;
     }
 }
 
-int checkInfix(char *infix, int length)
+bool checkInfix(const char *infix, const int length)
 {
     int i;
-    int isValid = 1;
+    bool isValid = true;
+
     for(i = 0; i < length; i++)
     {
-       if(isOperand(infix[i]) > 0
-          || isOperator(infix[i]) > 0
-          || infix[i] == '('
-          || infix[i] == ')'
-          || infix[i] == ' ')
-       {
-           continue;
-       }
-       else
-       {
-           isValid = -1;
-       }
+        if(isOperand(infix[i])
+           || isOperator(infix[i])
+           || infix[i] == '('
+           || infix[i] == ')'
+           || infix[i] == ' ')
+        {
+            continue;
+        }
+        else
+        {
+            isValid = false;
+            break;
+        }
     }
     return isValid;
 }
 
-void writeToFile(char *postfix, int length)
+void writeToFile(const char *postfix, const int length)
 {
     FILE *file = fopen("c:\\Temp\\output.txt", "w");
 
@@ -114,7 +91,7 @@ void writeToFile(char *postfix, int length)
     else
     {
         int i;
-        for(i = 0; i < length * 2; i++)
+        for(i = 0; i < length * MULTIPLIER; i++)
         {
             fprintf(file, "%c", postfix[i]);
             printf("%c", postfix[i]);
@@ -124,3 +101,23 @@ void writeToFile(char *postfix, int length)
     }
 }
 
+void generateReversePolishNotation(const char *infix, const int length)
+{
+    if(checkInfix(infix, length))
+    {
+        Stack_t *myStack = createStack();
+
+        push(myStack, '(');
+
+        char *postfix = parseInput(myStack, infix, length);
+
+        writeToFile(postfix, length);
+
+        free(postfix);
+        free(myStack);
+    }
+    else
+    {
+        printf("File contains wrong string %s\n", infix);
+    }
+}
