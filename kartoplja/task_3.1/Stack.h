06748c2b39279446bf77d9f5a39aba3e44ccaa81
KartashovaOlga <kartashovaolga96@gmail.com>
2018-08-20 22:57:17 +0300

    Update Stack.h

--- kartoplja/task_3.1/Stack.h
+++ kartoplja/task_3.1/Stack.h
@@ -1,22 +1,28 @@
 #ifndef STACK_H
 #define STACK_H
 
-#define LOWPRIORITY 1
-#define HIGHPRIORITY 2
-#define MAXSIZE 50
+#include <stdbool.h>
 
-typedef struct stack{
-    char stackArr[MAXSIZE];
+#define LOWPRIORITY 0
+#define HIGHPRIORITY 2
+#define SIZE 50
+#define MULTIPLIER 2
+
+typedef struct Stack{
+    char *stack_Data;
+    int size;
     int top;
 }Stack_t;
 
-void push(Stack_t *myStack, char symb);
+void push(Stack_t *myStack, const char symb);
 char pop(Stack_t *myStack);
 char getTop(Stack_t *myStack);
-int isEmpty(Stack_t *myStack);
-int isOperand(char symb);
-int isOperator(char symb);
-char *parseInput(Stack_t * myStack, char *infix, int length);
-int getPriority(char symb);
+bool isEmpty(Stack_t *myStack);
+bool isOperand(const char symb);
+bool isOperator(const char symb);
+char *parseInput(Stack_t * myStack, const char *infix, const int length);
+bool getPriority(const char symb);
+void resize(Stack_t *myStack);
+Stack_t * createStack(void);
 
 #endif
